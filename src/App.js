f5206e986f119c4f053ae080987ad0509960dfdd
Amy Pepper <amyandmicahpepper@gmail.com>
2020-07-16 16:37:17 -0700

    establish component file structure

--- src/App.js
+++ src/App.js
@@ -2,6 +2,23 @@ import React from "react";
 
 import "./App.css";
 
+const booksAPI = {
+  urls: [
+    "https://www.googleapis.com/books/v1/{collectionName}/resourceID?parameters",
+    "https://www.googleapis.com/books/v1/volumes?q=your-query-here&key=your-API-Key",
+  ],
+  params: [
+    "filter=free-ebooks",
+    "filter=partial",
+    "filter=full",
+    "filter=ebooks",
+    "filter=paid-ebooks",
+    "printType=all",
+    "printType=magazines",
+    "printType=books",
+  ],
+};
+
 function App() {}
 
 export default App;
